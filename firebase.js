116e5a9570671db5130cf7544e1df58dbef321fe
quangduy-rd <quangduyrd1@gmail.com>
2025-10-10 10:50:50 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// --- Cấu hình Firebase (thay giá trị thật vào dưới đây) ---
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// --- Khởi tạo Firebase ---
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
